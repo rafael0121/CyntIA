de18bd5cc5c30653d9370c0191c29c70892e7f6f
Marcus Leandro <marcusleandro12357@gmail.com>
2024-10-10 20:37:33 -0300

    Codigo de teste, esperando modulo para testar

---
+++ include/api.h
@@ -0,0 +1,8 @@
+#ifndef API_KEYS_H
+#define API_KEYS_H
+
+// Definições das chaves de API
+#define GOOGLE_API_KEY "AIzaSyC1CauypyqcfVLgqmM33XpMASEZ4mr5Ppo"
+
+
+#endif // API_KEYS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,54 @@
-#include <Arduino.h>
+#include "FS.h"
+#include "SPIFFS.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Defina o pino do DAC (GPIO25)
+#define DAC_PIN 25
+
+// Função para reproduzir o áudio WAV
+void playWAV(fs::FS &fs, const char *path) {
+    File file = fs.open(path);
+    
+    if (!file) {
+        Serial.println("Erro ao abrir o arquivo!");
+        return;
+    } else {
+        Serial.println("Abri");
+    }
+
+    // Lê e descarta o cabeçalho WAV (44 bytes)
+    for (int i = 0; i < 44; i++) {
+        file.read();
+    }
+
+    Serial.println("Tocar");
+    // Reproduz os dados de áudio
+    while (file.available()) {
+        uint8_t sample = file.read();  // Lê um byte do arquivo
+
+        // Escreve o valor no DAC (valores de 0 a 255)
+        dacWrite(DAC_PIN, sample);
+        Serial.println("Tocando");
+        // Adiciona um pequeno delay para controlar a taxa de amostragem
+        delayMicroseconds(62);  // Aproximadamente 8kHz
+    }
+
+    file.close();
+    Serial.println("Reprodução de áudio finalizada!");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Serial.println("teste");
+    // Inicializa o SPIFFS
+    if (!SPIFFS.begin(true)) {
+        Serial.println("Erro ao montar o sistema de arquivos SPIFFS!");
+        return;
+    }
+
+    // Inicia a reprodução do arquivo WAV
+    playWAV(SPIFFS, "/teste.wav");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Nada aqui, a reprodução é feita no setup.
 }
