3d96e5b62aa5713e6a7550d612d7c9cb50c522db
Marcus Leandro <marcusleandro12357@gmail.com>
2024-10-12 12:06:58 -0300

    commit inicial para testes do sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#define SENSOR_PIN 18  // Pin onde o KY-031 está conectado
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);  // Inicializa o Monitor Serial
+  pinMode(SENSOR_PIN, INPUT);  // Define o pino como entrada
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int estado = digitalRead(SENSOR_PIN);  // Lê o estado do sensor
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (estado == HIGH) {
+    Serial.println("Batida detectada!");  // Imprime se houve batida
+  } else {
+    Serial.println("Nenhuma batida.");  // Imprime se não houve batida
+  }
+
+  delay(500);  // Pequeno atraso para evitar spam no Monitor Serial
 }
